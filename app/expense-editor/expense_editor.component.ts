e92f0aaf116f74b09334ccd48e8128a37ccf0e5b
Tomi Virkki <virkki@vaadin.com>
2016-03-31 13:26:45 +0300

    Cleaned up expense editor

--- app/expense-editor/expense_editor.component.ts
+++ app/expense-editor/expense_editor.component.ts
@@ -6,79 +6,8 @@ import {VaadinElement} from '../vaadin-element/vaadin_element.directive';
 
 @Component({
   selector: 'expense-editor',
-  template: `
-    <div class="main-layout">
-      <h2>Edit expense</h2>
-      <paper-icon-button icon="close" (click)="close()" class="close-button self-start"></paper-icon-button>
-    </div>
-
-
-    <form (ngSubmit)="onSubmit($event.value)" #expenseForm="ngForm">
-      <div class="form">
-        <paper-input #merchant ngControl="merchant" ngDefaultControl [value]="expense.merchant" (value-changed)="merchant.fire('input');" label="Merchant" auto-validate required error-message="Merchant name required"></paper-input>
-        <paper-input #total ngControl="total" ngDefaultControl [value]="expense.total" (value-changed)="total.fire('input');" polymer-element label="Total" auto-validate required pattern="[0-9,.]+" error-message="Numeric values only">
-          <div prefix>$</div>
-        </paper-input>
-
-        <vaadin-date-picker ngControl="date" ngDefaultControl auto-validate required [value]="expense.date" label="Date"></vaadin-date-picker>
-
-        <paper-textarea #comment ngControl="comment" ngDefaultControl [value]="expense.comment" (value-changed)="comment.fire('input');" label="Comment"></paper-textarea>
-
-      </div>
-
-      <div class="receipt">
-        <input type="hidden" ngControl="receipt" [ngModel]="expense.receipt">
-        <vaadin-upload (upload-success)="uploadSuccess($event)">
-          <div class="file-list">
-            <img *ngIf="expense.receipt !== 'default'" src={{expense.receipt}}>
-            <img *ngIf="expense.receipt === 'default'" src="images/default-receipt.png">
-          </div>
-        </vaadin-upload>
-      </div>
-    </form>
-
-    <div class="buttons-layout">
-      <paper-button raised [disabled]="!expenseForm.form.valid" (click)="expenseForm.ngSubmit.emit(expenseForm)">Save</paper-button>
-      <paper-button (click)="close()">Cancel</paper-button>
-    </div>
-  `,
-  styles: [`
-      .main-layout {
-        display: flex;
-        justify-content: space-between;
-      }
-
-      paper-icon-button {
-        height: 28px;
-        width: 28px;
-      }
-
-      form {
-        display: flex;
-        flex: 1;
-        overflow: auto;
-      }
-
-      .form {
-        flex: 2;
-        padding-right: 24px;
-      }
-
-      .receipt {
-        flex: 3;
-        background: #F7F8F8;
-      }
-
-      img {
-        max-height: 500px;
-        margin-bottom: 20px;
-      }
-
-      vaadin-upload {
-        display: flex;
-        flex-direction: column-reverse;
-      }
-    `],
+  templateUrl: './app/expense-editor/expense_editor.component.html',
+  styleUrls: ['./app/expense-editor/expense_editor.component.css'],
   directives: [PolymerElement, VaadinElement]
 })
 export class ExpenseEditor {
