1c72e3b4635f4a8cf2cc0f7e7d7d27390ad28290
Teemu Pöntelin <teemu@vaadin.com>
2016-04-19 07:51:50 +0300

    Add vaadin-date-picker elements to the filters

--- app/search-filters/search_filters.component.js
+++ app/search-filters/search_filters.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', '../../bower_components/vaadin-combo-box/directives/vaadin-combo-box'], function(exports_1, context_1) {
+System.register(['angular2/core', '../../bower_components/vaadin-combo-box/directives/vaadin-combo-box', '../../bower_components/vaadin-date-picker/directives/vaadin-date-picker'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', '../../bower_components/vaadin-combo-box/direc
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, vaadin_combo_box_1;
+    var core_1, vaadin_combo_box_1, vaadin_date_picker_1;
     var SearchFilters;
     return {
         setters:[
@@ -19,6 +19,9 @@ System.register(['angular2/core', '../../bower_components/vaadin-combo-box/direc
             },
             function (vaadin_combo_box_1_1) {
                 vaadin_combo_box_1 = vaadin_combo_box_1_1;
+            },
+            function (vaadin_date_picker_1_1) {
+                vaadin_date_picker_1 = vaadin_date_picker_1_1;
             }],
         execute: function() {
             SearchFilters = (function () {
@@ -40,9 +43,9 @@ System.register(['angular2/core', '../../bower_components/vaadin-combo-box/direc
                 SearchFilters = __decorate([
                     core_1.Component({
                         selector: 'search-filters',
-                        template: "\n    <vaadin-combo-box class=\"merchants\" [items]=\"merchants\" label=\"Merchant\" (valueChange)=\"filtersChanged()\" [(value)]=\"filters.merchant\"></vaadin-combo-box>\n    <paper-input placeholder=\"Min\" (keyup)=\"filters.min = $event.target.value; filtersChanged();\"></paper-input>\n    <paper-input placeholder=\"Max\" (keyup)=\"filters.max = $event.target.value; filtersChanged();\"></paper-input>\n  ",
+                        template: "\n    <div class=\"row\">\n      <vaadin-date-picker label=\"After\" (valueChange)=\"filtersChanged()\" [(value)]=\"filters.after\"></vaadin-date-picker>\n      <vaadin-date-picker label=\"Before\" (valueChange)=\"filtersChanged()\" [(value)]=\"filters.before\"></vaadin-date-picker>\n      <vaadin-combo-box class=\"merchants\" [items]=\"merchants\" label=\"Merchant\" (valueChange)=\"filtersChanged()\" [(value)]=\"filters.merchant\"></vaadin-combo-box>\n    </div>\n    <div class=\"row\">\n      <paper-input placeholder=\"Min ($)\" (keyup)=\"filters.min = $event.target.value; filtersChanged();\"></paper-input>\n      <paper-input placeholder=\"Max ($)\" (keyup)=\"filters.max = $event.target.value; filtersChanged();\"></paper-input>\n    </div>\n  ",
                         styleUrls: ['./app/search-filters/search_filters.component.css'],
-                        directives: [vaadin_combo_box_1.VaadinComboBox]
+                        directives: [vaadin_combo_box_1.VaadinComboBox, vaadin_date_picker_1.VaadinDatePicker]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], SearchFilters);
--- app/search-filters/search_filters.component.ts
+++ app/search-filters/search_filters.component.ts
@@ -1,15 +1,22 @@
 import {Component, EventEmitter, Output, Input} from 'angular2/core';
-import { VaadinComboBox } from '../../bower_components/vaadin-combo-box/directives/vaadin-combo-box';
+import {VaadinComboBox} from '../../bower_components/vaadin-combo-box/directives/vaadin-combo-box';
+import {VaadinDatePicker} from '../../bower_components/vaadin-date-picker/directives/vaadin-date-picker';
 
 @Component({
   selector: 'search-filters',
   template: `
-    <vaadin-combo-box class="merchants" [items]="merchants" label="Merchant" (valueChange)="filtersChanged()" [(value)]="filters.merchant"></vaadin-combo-box>
-    <paper-input placeholder="Min" (keyup)="filters.min = $event.target.value; filtersChanged();"></paper-input>
-    <paper-input placeholder="Max" (keyup)="filters.max = $event.target.value; filtersChanged();"></paper-input>
+    <div class="row">
+      <vaadin-date-picker label="After" (valueChange)="filtersChanged()" [(value)]="filters.after"></vaadin-date-picker>
+      <vaadin-date-picker label="Before" (valueChange)="filtersChanged()" [(value)]="filters.before"></vaadin-date-picker>
+      <vaadin-combo-box class="merchants" [items]="merchants" label="Merchant" (valueChange)="filtersChanged()" [(value)]="filters.merchant"></vaadin-combo-box>
+    </div>
+    <div class="row">
+      <paper-input placeholder="Min ($)" (keyup)="filters.min = $event.target.value; filtersChanged();"></paper-input>
+      <paper-input placeholder="Max ($)" (keyup)="filters.max = $event.target.value; filtersChanged();"></paper-input>
+    </div>
   `,
   styleUrls: ['./app/search-filters/search_filters.component.css'],
-  directives: [VaadinComboBox]
+  directives: [VaadinComboBox, VaadinDatePicker]
 })
 export class SearchFilters {
 
