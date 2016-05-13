8414123c1532aa14a756abbf80297d9b4d612bf6
Manolo Carrasco <manolo@vaadin.com>
2016-05-13 18:05:28 +0200

    Use polymer from npm bundle instead of bower

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
-System.register(['@angular/platform-browser-dynamic', './expense_app.component'], function(exports_1) {
+System.register(['@angular/platform-browser-dynamic', './expense_app.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var platform_browser_dynamic_1, expense_app_component_1;
     return {
         setters:[
--- app/search-filters/search_filters.component.js
+++ app/search-filters/search_filters.component.js
@@ -1,11 +1,11 @@
-System.register(['@angular/core', '@vaadin/angular2-polymer'], function(exports_1) {
+System.register(['@angular/core', '@vaadin/angular2-polymer'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
-        switch (arguments.length) {
-            case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
-            case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
-            case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
-        }
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
@@ -51,11 +51,11 @@ System.register(['@angular/core', '@vaadin/angular2-polymer'], function(exports_
                 __decorate([
                     core_1.Output(), 
                     __metadata('design:type', Object)
-                ], SearchFilters.prototype, "filtersChange");
+                ], SearchFilters.prototype, "filtersChange", void 0);
                 __decorate([
                     core_1.Input(), 
                     __metadata('design:type', Array)
-                ], SearchFilters.prototype, "merchants");
+                ], SearchFilters.prototype, "merchants", void 0);
                 SearchFilters = __decorate([
                     core_1.Component({
                         selector: 'search-filters',
@@ -66,7 +66,7 @@ System.register(['@angular/core', '@vaadin/angular2-polymer'], function(exports_
                     __metadata('design:paramtypes', [])
                 ], SearchFilters);
                 return SearchFilters;
-            })();
+            }());
             exports_1("SearchFilters", SearchFilters);
         }
     }
